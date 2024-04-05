04f5f2ce4d30ee45b64ac2966e03185dde62be12
MairaTariq16 <maira.tariq@intechww.com>
2024-04-05 14:47:08 +0500

    feat: add cdn config and link with custom domain

--- main.tf
+++ main.tf
@@ -40,3 +40,89 @@ resource "azurerm_storage_blob" "blob" {
   source                 = "../cv-website/index.html"
 }
 
+
+resource "azurerm_cdn_profile" "cdn_profile" {
+  name                = "cloud-resume-static-cdn"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard_Microsoft"
+}
+
+
+resource "azurerm_cdn_endpoint" "endpoint" {
+  name                          = "cloud-resume-static"
+  profile_name                  = azurerm_cdn_profile.cdn_profile.name
+  location                      = azurerm_cdn_profile.cdn_profile.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  is_http_allowed               = true
+  is_https_allowed              = true
+  querystring_caching_behaviour = "IgnoreQueryString"
+  is_compression_enabled        = true
+  content_types_to_compress = [
+    "application/eot",
+    "application/font",
+    "application/font-sfnt",
+    "application/javascript",
+    "application/json",
+    "application/opentype",
+    "application/otf",
+    "application/pkcs7-mime",
+    "application/truetype",
+    "application/ttf",
+    "application/vnd.ms-fontobject",
+    "application/xhtml+xml",
+    "application/xml",
+    "application/xml+rss",
+    "application/x-font-opentype",
+    "application/x-font-truetype",
+    "application/x-font-ttf",
+    "application/x-httpd-cgi",
+    "application/x-javascript",
+    "application/x-mpegurl",
+    "application/x-opentype",
+    "application/x-otf",
+    "application/x-perl",
+    "application/x-ttf",
+    "font/eot",
+    "font/ttf",
+    "font/otf",
+    "font/opentype",
+    "image/svg+xml",
+    "text/css",
+    "text/csv",
+    "text/html",
+    "text/javascript",
+    "text/js",
+    "text/plain",
+    "text/richtext",
+    "text/tab-separated-values",
+    "text/xml",
+    "text/x-script",
+    "text/x-component",
+    "text/x-java-source",
+  ]
+  origin {
+    name      = "cloud-resume-storage-origin"
+    host_name = azurerm_storage_account.st.primary_web_host
+  }
+  delivery_rule {
+    name  = "EnforceHTTPS"
+    order = "1"
+
+    request_scheme_condition {
+      operator     = "Equal"
+      match_values = ["HTTP"]
+    }
+
+    url_redirect_action {
+      redirect_type = "Found"
+      protocol      = "Https"
+    }
+  }
+}
+
+resource "azurerm_cdn_endpoint_custom_domain" "custom_domain" {
+  name            = "personal-domain"
+  cdn_endpoint_id = azurerm_cdn_endpoint.endpoint.id
+  host_name       = "www.mairatariq.me"
+}
